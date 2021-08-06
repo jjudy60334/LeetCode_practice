489bd0946b8645642699e4a1d6296730bdfa9967
Claire Wu <jjudy708618@gmail.com>
2021-08-06 21:57:00 +0800

    refoctory

--- LeetCode/dynamic-programming/run.py
+++ LeetCode/dynamic-programming/run.py
@@ -14,35 +14,23 @@ class Parent:
     def right(self, node):
         self.right = node
 
-    def get_data(self, c):
+    def insert(self, n, k, l, c):
         self.c = c
-        if self.left or self.right:
-            if self.left:
-                self.left.get_data(self.c)
-            # print (self.value),
-            if self.right:
-                self.right.get_data(self.c)
-        else:
-            print(self.value)
-            self.c.append(self.value)
-            return self.c
-
-    def insert(self, n, k, l):
-
-        if k <= n and l < n and l <= k and (k == n and l == n) == False:
-            if k + 1 <= n:
+        if k <= n and l < n and l <= k:
+            if k < n:
                 self.left = Parent(self.value + "(")
-                self.left.insert(n, k + 1, l)
+                self.left.insert(n, k + 1, l, self.c)
             if l < k:
                 self.right = Parent(self.value + ")")
-                self.right.insert(n, k, l + 1)
+                self.right.insert(n, k, l + 1, self.c)
             else:
-                self.data = "("
+                self.value = "("
+        else:
+            self.c.append(self.value)
 
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         t = Parent()
-        t.insert(n, 0, 0)
-        t.get_data([])
+        t.insert(n, 0, 0, [])
         return t.combination()
