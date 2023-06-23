76ad4dc1c746dcf1ca1be9b9b572e8368aa59121
Claire Wu <jjudy708618@gmail.com>
2023-06-23 22:39:41 +0800

    update 70 to binary search

--- LeetCode/70. Climbing Stairs/run.py
+++ LeetCode/70. Climbing Stairs/run.py
@@ -4,3 +4,22 @@ class Solution:
         while n**2 <= x:
             n += 1
         return n - 1
+
+# Use binary search
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l = 0
+        r = x + 1
+        n = (l + r) // 2
+        while l < r:
+            n = (l + r) // 2
+            if n**2 <= x and (n + 1)**2 > x:
+                return n
+                break
+            elif n**2 > x:
+                r = n
+            else:
+                l = n
+        return n
