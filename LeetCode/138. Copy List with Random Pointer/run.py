725346b424881de527e04ded74be1288e04dbcb2
Claire Wu <jjudy708618@gmail.com>
2023-09-15 00:20:39 +0800

    add 138,linked list

---
+++ LeetCode/138. Copy List with Random Pointer/run.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        mapping = {}
+        cur = head
+        if not head:
+            return None
+        while cur:
+            mapping[cur] = Node(cur.val)
+            cur = cur.next
+
+        cur = head
+        while cur:
+            if cur.next:
+                mapping[cur].next = mapping[cur.next]
+            if cur.random:
+                mapping[cur].random = mapping[cur.random]
+            cur = cur.next
+
+        return mapping[head]
