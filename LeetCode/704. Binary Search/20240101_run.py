e181836977842d9d7f7a83c6e0f66f38bb6d9caf
Claire Wu <jjudy708618@gmail.com>
2024-01-01 17:35:13 +0800

    704 add recursive

--- LeetCode/704. Binary Search/20240101_run.py
+++ LeetCode/704. Binary Search/20240101_run.py
@@ -21,3 +21,27 @@ class Solution:
                 return m
 
         return -1
+
+
+# recursive
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)-1
+
+        def binary_search(l, r):
+            if l > r:
+                return -1
+            m = (l+r)//2
+            n_m = nums[m]
+            if nums[l] == target:
+                return l
+            if nums[r] == target:
+                return r
+            if target == n_m:
+                return m
+            elif target < n_m:
+                return binary_search(l+1, m)
+            else:
+                return binary_search(m, r-1)
+        return binary_search(l, r)
