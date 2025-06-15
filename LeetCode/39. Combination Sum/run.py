7e94d4a18f508e07aaadf3e03e5ab024902648fa
Claire Wu <jjudy708618@gmail.com>
2025-06-15 18:44:00 +0800

    add 39

---
+++ LeetCode/39. Combination Sum/run.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        count_r = []
+
+        def count_f(l):
+            return {i: l.count(i) for i in set(l)}
+
+        def combination(root, candidates, target, comb):
+            if target == 0:
+                if count_f(comb) not in count_r:
+                    result.append(comb)
+                    count_r.append(count_f(comb))
+                else:
+                    return None
+            else:
+                for i in candidates:
+                    combination(i, [k for k in candidates if k <=
+                                target-i], target-i, comb+[i])
+
+        combination(0, candidates, target, [])
+        return result
